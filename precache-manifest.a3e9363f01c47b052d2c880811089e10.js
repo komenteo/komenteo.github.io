4f50405dbcc39651e3ad4ae3826b46e77b7ffcd5
Ugo Jardonnet <ugodotjardonnetatgmail>
2021-01-21 13:35:16 +0100

    Updates

---
+++ precache-manifest.a3e9363f01c47b052d2c880811089e10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0927441d2b90b8c9056db19c9bdf8c5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f31aee1f37cd74fafd3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "11ff32251ee1d44af983",
+    "url": "/static/js/2.41dedf1c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.41dedf1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f31aee1f37cd74fafd3",
+    "url": "/static/js/main.4c20fbda.chunk.js"
+  },
+  {
+    "revision": "54d9c82f737a062e7377",
+    "url": "/static/js/runtime-main.ac7c84ba.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.efa57d4a20fc34eb022a49c4a7007aa9.js"
+  "/precache-manifest.a3e9363f01c47b052d2c880811089e10.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
